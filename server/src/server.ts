a9a7b4ef3c871ef4a7f83f568f56a97cea909cdd
Drew Osmond <drewosmond@gmail.com>
2021-10-07 16:11:23 -0700

    changed how I was sending the html file

--- server/src/server.ts
+++ server/src/server.ts
@@ -51,7 +51,7 @@ const startConnection = async () => {
 
     app.get(/^(?!\/?api).*/, (req, res) => {
       res.cookie("XSRF-TOKEN", req.csrfToken());
-      res.sendFile(path.resolve(__dirname, "../../web", "build", "index.html"));
+      res.sendFile(path.join(__dirname, "index.html"));
     });
   }
 };
