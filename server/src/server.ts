dcb6d29486c2f42828e29885185580b136333016
Drew Osmond <drewosmond@gmail.com>
2021-10-07 15:59:01 -0700

    changed how I was sending the html file

--- server/src/server.ts
+++ server/src/server.ts
@@ -42,7 +42,9 @@ const startConnection = async () => {
   if (process.env.NODE_ENV === "production") {
     app.get("/", (req, res) => {
       res.cookie("XSRF-TOKEN", req.csrfToken());
-      res.sendFile(path.resolve(__dirname, "../web", "build", "index.html"));
+      res.sendFile(
+        path.resolve(__dirname, "../../web/public", "build", "index.html")
+      );
     });
 
     router.use(express.static(path.resolve("../web/build")));
